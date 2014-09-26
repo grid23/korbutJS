0f134c3ead92cf9c81f3a294eeb56adefa7dd2ad
Grid23 <hello@grid23.net>
2014-09-26 11:19:48 +0200

    oc

---
+++ src/requestAnimationFrame.js
@@ -0,0 +1,4 @@
+"use strict"
+
+console.warn("korbutjs/src/requestAnimationFrame is deprecated, require korbutjs/src/dom-utils/requestAnimationFrame")
+module.exports = require("./dom-utils/requestAnimationFrame")
